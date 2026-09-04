d27041309a15350d5e2085bb4a1420fdeee6d526
AntoniousMamdouh <am.antonious@gmail.com>
2026-09-04 17:58:01 +0300

    practice . operator in dart

---
+++ dotOperator.dart
@@ -0,0 +1,23 @@
+void main() {
+  int x = 9;
+  print(x.isOdd);
+  print(x.isEven);
+  print(x.abs()); //القيمة المطلقة للرقم
+  print(x.toDouble());
+  double y = 12.5;
+  print(y.ceil());
+  print(y.floor());
+  print(y.round());
+  print(y.runtimeType); // لبيان نوع المتغير
+  print(y.compareTo(13));
+  print(y.remainder(4)); // باقى العدد زى ال modulus
+  print(y.toInt());
+  print(y.toString());
+  String s = "tony";
+  print(s.isEmpty); //تشتغل مع النصوص فقط
+  print(s.isNotEmpty);
+  print(s.toLowerCase());
+  print(s.toUpperCase());
+  print(s.length);
+  print(int.tryParse(s)); // للتغلب على مشكلة تحويل النص الى رقم هيطلعلى القيمة null
+} 
