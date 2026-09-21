8caecc2e3b73e952fbaf30e91e3e4abebec77158
AntoniousMamdouh <am.antonious@gmail.com>
2026-09-22 00:02:41 +0300

    creat for loop

---
+++ loop.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+void main() {
+  //// ============ Loop in Dart ============ ////
+  ///for loop              (dataType starting; condition; increment || decrement;)
+  ///while loop            (dataType starting; condition; increment || decrement;)
+  ///do while loop         (dataType starting; condition; increment || decrement;)
+
+  ///for each loop         with array types
+  ///for in loop           with array types
+  //=======================================================//
+  //1) for loop
+  // for (int i = 10; i <= 100; i += 10) {
+  //   print(i); // 10 20 30 40 ...
+  // }
+
+  // for (int x = 100; x >= 10; x -= 5) {
+  //   print(x); // 100 90 80 70 ...
+  // }
+  //  10 جدول الضرب
+  // for (int z = 1; z <= 20; z++) {
+
+  //   print("10 X $z = ${z * 10}");
+  // }
+  // print("Enter Number: ");
+  // int h = int.parse(stdin.readLineSync()!);
+  // for (int i = 0; i <= h; i+=5) {
+  //   print(i);
+  // }
+  //رسم مثلث نجوم
+  // for (int i = 5; i >=1 ; i--) {
+  //   print("*" * i);
+  // }
+//   for (int i = 1; i <= 10; i++) {
+//   print(" " * (10 - i) + "*" * (2 * i - 1));
+// }
+}
